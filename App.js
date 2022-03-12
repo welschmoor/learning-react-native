9235c178f61ba32bfd9b49d12a0b82b37681fd6d
Johannes Welsch <welschmoor@gmail.com>
2022-03-12 11:19:28 +0100

    eslint added

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>App!</Text>
+      <Text>App! e</Text>
       <StatusBar style="auto" />
     </View>
   );
